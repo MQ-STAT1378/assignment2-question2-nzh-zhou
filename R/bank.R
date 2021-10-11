e3d9b838cd12acdb2fd61d14979f532b6de17c6a
Ze Hong Zhou <nzh.zhou@outlook.com>
2021-10-11 17:40:30 +1100

    added bank data

---
+++ R/bank.R
@@ -0,0 +1,10 @@
+#' Simulation of the customer queuing system of a bank
+#'
+#' A dataset containing the customer arrival time and service time
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{arrival_time}{arrival time of a customer, in seconds}
+#'   \item{service_time}{service time of a customer, in seconds}
+#' }
+"bank"
